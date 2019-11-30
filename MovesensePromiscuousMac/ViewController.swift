0e1c2c9f404dcbdb9b2c58cda22246cfb9f47215
Mayur <mayurbhandary@gmail.com>
2019-11-30 17:36:40 -0500

    Added support for Movesense part numbers in scan response packet and battery level reporting

--- MovesensePromiscuousMac/ViewController.swift
+++ MovesensePromiscuousMac/ViewController.swift
@@ -10,7 +10,13 @@ import Cocoa
 import CoreBluetooth
 
 class ViewController: NSViewController, CBCentralManagerDelegate,CBPeripheralDelegate {
+    
     private var centralManager: CBCentralManager!
+    
+    private var uuid_to_movesense_cache: [String: String] = [:] //STORED AS UUID:MOVESENSEID
+    private var movesense_to_uuid_cache: [String: String] = [:] //STORED AS MOVESENSEID:UUID
+    private var movesense_to_data_cache: [String: (Float,String)] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //Create a DispatchQueue to instantiate a CBCentralManager Object. This allows us to search for Bluetooth devices in the on a separate thread
@@ -48,26 +54,71 @@ class ViewController: NSViewController, CBCentralManagerDelegate,CBPeripheralDel
     //Called when a peripheral device is discovered
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
 
-    
+        //print(peripheral.identifier.uuidString)
+
         var bytes=[UInt8](repeating:0, count:16)
         var hr_bytes=[UInt8](repeating:0, count:4)
-        print(advertisementData)
+        var batt_byte=[UInt8](repeating:0, count:1)
+        //print(advertisementData)
         
-        if advertisementData["kCBAdvDataManufacturerData"] != nil{
-            let payload: NSData = advertisementData["kCBAdvDataManufacturerData"]! as! NSData
-            print(payload)
-            
-            payload.getBytes(&bytes,length:16)
-            //let array=[UInt8](payload)
-            
-            print(bytes)
-            var hr:Float = 0
-            for i in 7...10{
-                hr_bytes[i-7]=bytes[i]
+        if uuid_to_movesense_cache[peripheral.identifier.uuidString] != nil{// We have seen this UUID before
+            if advertisementData["kCBAdvDataManufacturerData"] != nil{ //We received heart rate data
+                let payload: NSData = advertisementData["kCBAdvDataManufacturerData"]! as! NSData
+                //print(payload)
+                
+                payload.getBytes(&bytes,length:15)
+                //let array=[UInt8](payload)
+                
+                //print(bytes)
+                var hr:Float = 0
+                var batt:UInt8 = 0
+                var batt_level = ""
+                for i in 7...10{
+                    hr_bytes[i-7]=bytes[i]
+                }
+                
+                batt_byte[0]=bytes[14]
+                memcpy(&hr,&hr_bytes,4)
+                memcpy(&batt,&batt_byte,1)
+                if batt == 1 {
+                    batt_level="Battery Full"
+                }
+                else{
+                    batt_level="Battery Low"
+                }
+                
+                
+                movesense_to_data_cache[uuid_to_movesense_cache[peripheral.identifier.uuidString] as! String] = (hr,batt_level)
+                
+                
             }
-            memcpy(&hr,&hr_bytes,4)
-            print(hr)
         }
+        else { //We have not seen this UUID before
+            if advertisementData["kCBAdvDataLocalName"] != nil{//We received Movesense id
+                
+                let movesense_id = advertisementData["kCBAdvDataLocalName"] as! String
+                
+                //Only add a new entry in the tables if the movesense id has never been seen before
+                if movesense_to_uuid_cache[movesense_id] == nil{
+                    uuid_to_movesense_cache[peripheral.identifier.uuidString] = movesense_id
+                    movesense_to_uuid_cache[movesense_id] = peripheral.identifier.uuidString
+                }
+                    
+                //Otherwise, the UUID for movesense device has changed and we need to update it in the tables.
+                else{
+                    let old_uuid = movesense_to_uuid_cache[movesense_id] as! String
+                    movesense_to_uuid_cache[movesense_id]=peripheral.identifier.uuidString
+                    uuid_to_movesense_cache[old_uuid] = nil
+                }
+                
+            }
+            
+        }
+        
+        print("\(movesense_to_data_cache as AnyObject)")
+       
+        
+        
     }
 }
 
